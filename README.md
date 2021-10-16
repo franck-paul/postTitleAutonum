eccdc07b425062cb51a26a0ec420e46958dcf57c
franck <carnet.franck.paul@gmail.com>
2021-10-16 14:35:20 +0200

    First release 0.1

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# postTitleAutonum
-Dotclear 2 plugin
+postTitleAutonum plugin for Dotclear 2
+============================
+
+This plugin auto numbering duplicate entries titles at creation (only)
+
+The author should verify the modified title and URL provided by this plugin before publishing it.
+
+Author: Franck Paul
+Web: https://open-time.net/
---
+++ _define.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @brief postTitleAutonum, a plugin for Dotclear 2
+ *
+ * @package Dotclear
+ * @subpackage Plugins
+ *
+ * @author Franck Paul
+ *
+ * @copyright Franck Paul carnet.franck.paul@gmail.com
+ * @copyright GPL-2.0 https://www.gnu.org/licenses/gpl-2.0.html
+ */
+if (!defined('DC_RC_PATH')) {
+    return;
+}
+
+$this->registerModule(
+    'postTitleAutonum',                         // Name
+    'Auto numbering of duplicate titles',       // Description
+    'Franck Paul',                              // Author
+    '0.1',                                      // Version
+    [
+        'requires'    => [['core', '2.13']],                                // Dependencies
+        'permissions' => 'admin',                                           // Permissions
+        'type'        => 'plugin',                                          // Type
+        'details'     => 'https://open-time.net/?q=postTitleAutonum',       // Details URL
+        'support'     => 'https://github.com/franck-paul/postTitleAutonum', // Support URL
+        'settings'    => ['blog' => '#params.pta']                          // Settings
+    ]
+);
