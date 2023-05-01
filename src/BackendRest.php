8d5a3f950c48e05ff85913462990779a3ae8d0e7
franck <carnet.franck.paul@gmail.com>
2023-05-01 16:43:00 +0200

    Switch to SqlStatement for queries

--- src/BackendRest.php
+++ src/BackendRest.php
@@ -16,47 +16,60 @@ namespace Dotclear\Plugin\postTitleAutonum;
 
 use dcBlog;
 use dcCore;
-use Dotclear\Database\MetaRecord;
+use Dotclear\Database\Statement\SelectStatement;
 
 class BackendRest
 {
     private static function getTitle($title, $type = 'post')
     {
-        $strReq = 'SELECT post_title FROM ' . dcCore::app()->blog->prefix . dcBlog::POST_TABLE_NAME . ' ' .
-        "WHERE post_title = '" . dcCore::app()->blog->con->escape($title) . "' " .
-        "AND post_type = '" . dcCore::app()->blog->con->escape($type) . "' " .
-        "AND blog_id = '" . dcCore::app()->blog->con->escape(dcCore::app()->blog->id) . "' " .
-        'ORDER BY post_title DESC';
+        $sql = new SelectStatement();
+        $sql
+            ->column('post_title')
+            ->from(dcCore::app()->blog->prefix . dcBlog::POST_TABLE_NAME)
+            ->where('post_title = ' . $sql->quote($title))
+            ->and('post_type = ' . $sql->quote($type))
+            ->and('blog_id = ' . $sql->quote(dcCore::app()->blog->id))
+            ->order('post_title DESC')
+        ;
 
-        $rs = new MetaRecord(dcCore::app()->blog->con->select($strReq));
+        $rs = $sql->select();
+        if (!is_null($rs) && !$rs->isEmpty()) {
+            $sql = new SelectStatement();
 
-        if (!$rs->isEmpty()) {
-            // Try to find similar titles (beginning with)
-            if (dcCore::app()->blog->con->syntax() == 'mysql') {
-                $clause = "REGEXP '^" . dcCore::app()->blog->con->escape(preg_quote($title)) . " '";
-            } elseif (dcCore::app()->blog->con->driver() == 'pgsql') {
-                $clause = "~ '^" . dcCore::app()->blog->con->escape(preg_quote($title)) . " '";
+            // Try to find similar titles (beginning with, including a space)
+            if ($sql->syntax == 'mysql') {
+                // MySQL
+                $clause = "REGEXP '^" . $sql->escape(preg_quote($title)) . " '";
+            } elseif ($sql->syntax == 'postgresql') {
+                // PostgreSQL
+                $clause = "~ '^" . $sql->escape(preg_quote($title)) . " '";
             } else {
+                // SQlite
                 $clause = "LIKE '" .
-                dcCore::app()->blog->con->escape(preg_replace(['%', '_', '!'], ['!%', '!_', '!!'], $title)) . " ' ESCAPE '!'";  // @phpstan-ignore-line
+                $sql->escape(preg_replace(['%', '_', '!'], ['!%', '!_', '!!'], $title)) . " ' ESCAPE '!'";  // @phpstan-ignore-line
             }
-            $strReq = 'SELECT post_title FROM ' . dcCore::app()->blog->prefix . dcBlog::POST_TABLE_NAME . ' ' .
-            'WHERE post_title ' . $clause . ' ' .
-            "AND post_type = '" . dcCore::app()->blog->con->escape($type) . "' " .
-            "AND blog_id = '" . dcCore::app()->blog->con->escape(dcCore::app()->blog->id) . "' " .
-            'ORDER BY post_title DESC ';
 
-            $rs = new MetaRecord(dcCore::app()->blog->con->select($strReq));
+            $sql
+                ->column('post_title')
+                ->from(dcCore::app()->blog->prefix . dcBlog::POST_TABLE_NAME)
+                ->where('post_title ' . $clause)
+                ->and('post_type = ' . $sql->quote($type))
+                ->and('blog_id = ' . $sql->quote(dcCore::app()->blog->id))
+                ->order('post_title DESC')
+            ;
+
+            $rs = $sql->select();
             $a  = [];
             while ($rs->fetch()) {
                 $a[] = $rs->post_title;
             }
 
-            natsort($a);
-            if (preg_match('/(.*?)(\d+)$/', end($a), $m)) {
-                $i = (int) $m[2];
-            } else {
-                $i = 1;
+            $i = 1;
+            if (count($a)) {
+                natsort($a);
+                if (preg_match('/(.*?)(\d+)$/', end($a), $m)) {
+                    $i = (int) $m[2];
+                }
             }
 
             if ($i > 0) {
