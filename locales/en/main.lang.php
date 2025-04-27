01ebfa6a216c6529b021ea6431be400989d0d2c1
franck <carnet.franck.paul@gmail.com>
2025-04-27 16:22:55 +0200

    l10n

--- locales/en/main.lang.php
+++ locales/en/main.lang.php
@@ -10,11 +10,11 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Auto numbering of duplicate titles'] = '';
 L10n::$locales['postTitleAutonum'] = '';
+L10n::$locales['Auto numbering of duplicate titles'] = '';
 L10n::$locales['Enable auto numbering of duplicate titles'] = '';
 L10n::$locales['Use prefix before number'] = '';
 L10n::$locales['User defined prefix:'] = '';
 L10n::$locales['#'] = '';
 L10n::$locales['Leave empty to use the default prefix:'] = '';
-L10n::$locales['The “%s” title is already used, would you replace it by “%s”?'] = '';
+L10n::$locales['The “%1$s” title is already used, would you replace it by “%2$s”?'] = '';
--- locales/fr/main.lang.php
+++ locales/fr/main.lang.php
@@ -10,11 +10,11 @@
 
 use Dotclear\Helper\L10n;
 
-L10n::$locales['Auto numbering of duplicate titles'] = 'Numérotation automatique des titres dupliqués';
 L10n::$locales['postTitleAutonum'] = 'Numérotation automatique des titres dupliqués';
+L10n::$locales['Auto numbering of duplicate titles'] = 'Numérotation automatique des titres dupliqués';
 L10n::$locales['Enable auto numbering of duplicate titles'] = 'Activer la numérotation automatique des titres dupliqués';
 L10n::$locales['Use prefix before number'] = 'Utiliser un préfixe devant le numéro';
 L10n::$locales['User defined prefix:'] = 'Préfixe :';
 L10n::$locales['#'] = 'n° ';
 L10n::$locales['Leave empty to use the default prefix:'] = 'Laisser vide pour utiliser le préfixe par défaut :';
-L10n::$locales['The “%s” title is already used, would you replace it by “%s”?'] = 'Le titre « %s » est déjà utilisé, voulez-vous le remplacer par « %s » ?';
+L10n::$locales['The “%1$s” title is already used, would you replace it by “%2$s”?'] = 'Le titre "%1$s" est déjà utilisé, le remplaceriez-vous par "%2$s" ?';
